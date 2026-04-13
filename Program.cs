b91f1c7020259b87aa8262cbfc5b15a6ed5f8fb1
vamshi26 <vamshichiliveri1999@gmail.com>
2026-04-12 23:44:17 -0500

    small change

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all 3 of the menu choices" };
 int stringsCount = myStrings.Length;
 string myString = "";
 int periodLocation = 0;
